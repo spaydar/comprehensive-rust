9e9e82b23f2f1ac1a3fbd1c19ff91fa589dd4813
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2023-09-24 00:17:16 -0700

    start 'Polygon Struct' exercise

--- src/main.rs
+++ src/main.rs
@@ -312,6 +312,24 @@ pub fn prefix_matches(prefix: &str, request_path: &str) -> bool {
     true
 }
 
+// The solution seems to assume that a wildcard can only match one path segment.
+// My implementation above works for cases where a wildcard can match an arbitrary
+// number of path segments i.e. multiple directories
+pub fn prefix_matches_solution(prefix: &str, request_path: &str) -> bool {
+
+    let mut request_segments = request_path.split('/');
+
+    for prefix_segment in prefix.split('/') {
+        let Some(request_segment) = request_segments.next() else {
+            return false;
+        };
+        if request_segment != prefix_segment && prefix_segment != "*" {
+            return false;
+        }
+    }
+    true
+}
+
 // A Simple GUI Library
 pub trait Widget {
     /// Natural width of `self`.
@@ -436,23 +454,114 @@ fn a_simple_gui_library_27_1() {
     window.draw();
 }
 
-// The solution seems to assume that a wildcard can only match one path segment.
-// My implementation above works for cases where a wildcard can match an arbitrary
-// number of path segments i.e. multiple directories
-pub fn prefix_matches_solution(prefix: &str, request_path: &str) -> bool {
-
-    let mut request_segments = request_path.split('/');
-
-    for prefix_segment in prefix.split('/') {
-        let Some(request_segment) = request_segments.next() else {
-            return false;
-        };
-        if request_segment != prefix_segment && prefix_segment != "*" {
-            return false;
-        }
-    }
-    true
+// Polygon Struct
+pub struct Point {
+    x: i32,
+    y: i32
 }
+
+impl Point {
+    fn new(x: i32, y: i32) -> Point {
+        Point { x, y }
+    }
+
+    fn magnitude(&self) -> f64 {
+        f64::from(self.x.pow(2) + self.y.pow(2)).sqrt()
+    }
+}
+
+pub struct Polygon {
+    // add fields
+}
+
+impl Polygon {
+    // add methods
+}
+
+pub struct Circle {
+    // add fields
+}
+
+impl Circle {
+    // add methods
+}
+
+pub enum Shape {
+    Polygon(Polygon),
+    Circle(Circle),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn round_two_digits(x: f64) -> f64 {
+        (x * 100.0).round() / 100.0
+    }
+
+    #[test]
+    fn test_point_magnitude() {
+        let p1 = Point::new(12, 13);
+        assert_eq!(round_two_digits(p1.magnitude()), 17.69);
+    }
+
+    #[test]
+    fn test_point_dist() {
+        let p1 = Point::new(10, 10);
+        let p2 = Point::new(14, 13);
+        assert_eq!(round_two_digits(p1.dist(p2)), 5.00);
+    }
+
+    #[test]
+    fn test_point_add() {
+        let p1 = Point::new(16, 16);
+        let p2 = p1 + Point::new(-4, 3);
+        assert_eq!(p2, Point::new(12, 19));
+    }
+
+    #[test]
+    fn test_polygon_left_most_point() {
+        let p1 = Point::new(12, 13);
+        let p2 = Point::new(16, 16);
+
+        let mut poly = Polygon::new();
+        poly.add_point(p1);
+        poly.add_point(p2);
+        assert_eq!(poly.left_most_point(), Some(p1));
+    }
+
+    #[test]
+    fn test_polygon_iter() {
+        let p1 = Point::new(12, 13);
+        let p2 = Point::new(16, 16);
+
+        let mut poly = Polygon::new();
+        poly.add_point(p1);
+        poly.add_point(p2);
+
+        let points = poly.iter().cloned().collect::<Vec<_>>();
+        assert_eq!(points, vec![Point::new(12, 13), Point::new(16, 16)]);
+    }
+
+    #[test]
+    fn test_shape_perimeters() {
+        let mut poly = Polygon::new();
+        poly.add_point(Point::new(12, 13));
+        poly.add_point(Point::new(17, 11));
+        poly.add_point(Point::new(16, 16));
+        let shapes = vec![
+            Shape::from(poly),
+            Shape::from(Circle::new(Point::new(10, 20), 5)),
+        ];
+        let perimeters = shapes
+            .iter()
+            .map(Shape::perimeter)
+            .map(round_two_digits)
+            .collect::<Vec<_>>();
+        assert_eq!(perimeters, vec![15.48, 31.42]);
+    }
+}
+
 // Tests
 // Luhn tests
 #[test]
