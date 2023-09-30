ddc3d66eae6211000d905c1e8666c3619abf2ac1
Seth Paydar <29551413+spaydar@users.noreply.github.com>
2023-09-30 00:03:21 -0700

    start Dining Philosophers

--- src/main.rs
+++ src/main.rs
@@ -746,6 +746,66 @@ impl Drop for DirectoryIterator {
     }
 }
 
+// Dining Philosophers
+fn dining_philosophers_52_1() {
+    // Create forks
+    let forks = [Fork, Fork, Fork, Fork];
+
+    // Create philosophers
+    let (tx, rx) = mpsc::channel();
+    let mut philosophers: Vec<Philosopher> = vec![];
+    for i in 0..PHILOSOPHERS.len() {
+        philosophers.push(
+            Philosopher {
+                name: PHILOSOPHERS[i].to_owned(),
+                left_fork: &forks[i],
+                right_fork: &forks[(i + 1) % PHILOSOPHERS.len()],
+                thoughts: tx.clone()
+            }
+        );
+    }
+
+    // Make each of them think and eat 100 times
+    for p in philosophers {
+
+    }
+
+    // Output their thoughts
+    for msg in rx.iter() {
+        println!("{msg}");
+    }
+}
+
+use std::sync::{mpsc, Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
+struct Fork;
+
+struct Philosopher {
+    name: String,
+    left_fork: &Fork,
+    right_fork: &Fork,
+    thoughts: mpsc::Sender<String>
+}
+
+impl Philosopher {
+    fn think(&self) {
+        self.thoughts
+            .send(format!("Eureka! {} has a new idea!", &self.name))
+            .unwrap();
+    }
+
+    fn eat(&self) {
+        // Pick up forks...
+        println!("{} is eating...", &self.name);
+        thread::sleep(Duration::from_millis(10));
+    }
+}
+
+static PHILOSOPHERS: &[&str] =
+    &["Socrates", "Plato", "Aristotle", "Thales", "Pythagoras"];
+
 // Tests
 // Luhn tests
 #[test]
